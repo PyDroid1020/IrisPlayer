d8c0d67e3051822976b28781ba8d504c82934714
Droid <102081945+PyDroid1020@users.noreply.github.com>
2025-11-07 03:23:25 +0300

    Update buttons.py

--- source/ui/components/buttons.py
+++ source/ui/components/buttons.py
@@ -1,18 +1,18 @@
 import flet as ft
-from ...theme import DARK_ACCENT,PLAYER_BUTTONS_COLORS
+from ...theme import DARK_ACCENT,PLAYER_BUTTONS_COLORS,PLAYER_SPECIAL_BUTTONS_COLORS
 
 PLAY_BUTTON_ICON = ft.Icons.PLAY_ARROW
 
 def getButtons(player):
     icon_size = 30
     buttons = [
-        ft.IconButton(icon=ft.Icons.SHUFFLE_OUTLINED, icon_color=PLAYER_BUTTONS_COLORS, icon_size=icon_size ,on_click=player.toggle_shuffle),
+        ft.IconButton(icon=ft.Icons.SHUFFLE_OUTLINED, icon_color=PLAYER_SPECIAL_BUTTONS_COLORS, icon_size=icon_size ,on_click=player.toggle_shuffle),
         ft.IconButton(icon=ft.Icons.SKIP_PREVIOUS_ROUNDED, icon_color=PLAYER_BUTTONS_COLORS, icon_size=icon_size, on_click=player.previous),
         ft.IconButton(icon=ft.Icons.FAST_REWIND_ROUNDED, icon_color=PLAYER_BUTTONS_COLORS, icon_size=icon_size, on_click=player.seek_backward),
-        ft.IconButton(icon=PLAY_BUTTON_ICON, icon_color=PLAYER_BUTTONS_COLORS, icon_size=icon_size ,on_click=player.toggle_play),
+        ft.IconButton(icon=ft.Icons.PLAY_ARROW, icon_color=PLAYER_SPECIAL_BUTTONS_COLORS, icon_size=icon_size ,on_click=player.toggle_play),
         ft.IconButton(icon=ft.Icons.FAST_FORWARD_ROUNDED, icon_color=PLAYER_BUTTONS_COLORS, icon_size=icon_size, on_click=player.seek_forward),
         ft.IconButton(icon=ft.Icons.SKIP_NEXT_ROUNDED, icon_color=PLAYER_BUTTONS_COLORS, icon_size=icon_size, on_click=player.next),
-        ft.IconButton(icon=ft.Icons.REPEAT_OUTLINED, icon_color=PLAYER_BUTTONS_COLORS, icon_size=icon_size ,on_click=player.toggle_loop),
+        ft.IconButton(icon=ft.Icons.REPEAT_OUTLINED, icon_color=PLAYER_SPECIAL_BUTTONS_COLORS, icon_size=icon_size ,on_click=player.toggle_loop),
     ]
     return buttons
 
